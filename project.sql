cd46092c254bf275df5e7d7baddc391f47b7a035
Lavoisier Cornerstone <vois.cornerstone@gmail.com>
2013-12-17 02:13:20 -0500

    initial commit

---
+++ project.sql
@@ -0,0 +1,41 @@
+CREATE TABLE project (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  title TEXT
+  category TEXT
+  funding_goal
+  start_date TEXT
+  end_date TEXT
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  name TEXT
+  age INT
+);
+
+
+CREATE TABLE pledges (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  amount NUMERIC
+  user_id INT
+  project_id INT
+);
+
+
+
+-- Create the database
+-- You are going to make a database to store data for your crowd-funding platform. The platform needs to have projects, users, and pledges.
+
+-- Create a schema based on the following information:
+
+-- A project has a title, a category, a funding goal, a start date, and an end date. Valid categories are: music, books, charity.
+
+-- A user has a name and an age
+
+-- A pledge has an amount. It belongs to a user, and it also belongs to a project.
+
+-- Insert some records into the datbase
+
+-- Create 10 projects, 20 users, and 30 pledges.
+
+
---
+++ records.sql
@@ -0,0 +1,50 @@
+INSERT INTO projects (title, category ,funding_goal, start_date ,end_date)
+VALUES ('Ruby For Everyone Else', 'Publishing', '$12,000', '7/25/13', '11/25/13')
+('Fast Life', 'Film & Video', '$25,000', '9/23/13', '1/15/14')
+('Swart Smart Watch', 'Technology', '$65,000', '9/1/13', '3/15/14')
+('Radicals & Visionaries', 'Fashion', '$12,500', '1/5/14', '4/1/14')
+('Gods Green Earth', 'Art', '$10,000', '8/7/13','9/7/13')
+('Teach Kids Code', 'Education', '$30,000', '11/3/13', '3/1/14')
+('Grow The Heck Up', 'Film & Video', '$50,000', '12/15/13', '4/1/14')
+('Food Karma', 'Food', '$75,000', '6/1/13', '10/1/13')
+('Coats For Vets', 'Philanthropy', '$7,000', '11/1/13','12/15/13' )
+('x-TV', 'Technology', '$100,000', '11/20/13', '3/5/14' );
+
+
+
+INSERT INTO users (name,age)
+VALUES ('CaptainOG', 34)
+('Dr.Suess', 51),('RubyMonger' 31), ('Smurf', 27), ('PawzOnEm', 40), ('RapGod', 25), ('SuperTroll', 29), ('Dagnabit', 59), ('TerminalAnswer', 33), ('Violator', 22), ('AllWinsNoLoss', 30), ('QuoteTheRaven', 24), ('KickmeSpears', 22), ('MileyVirus', 30), ('WestCoasta', 41)
+('Aslan', 18),('CookieStomper' 24), ('VillageIdiot', 27), ('YoungVois', 34), ('MaxHeadRoom', 35),  
+
+INSERT INTO pledges (title, category, user_id, project_id)
+VALUES (75.00, 7, 8),
+(75.00, 20, 9),
+(75.00, 12, 1),
+(75.00, 1, 4),
+(75.00, 7, 6),
+(75.00, 4, 3),
+(500.00, 20, 3),
+(75.00, 13, 9),
+(75.00, 2, 4),
+(75.00, 1, 7),
+(75.00, 9, 8),
+(1000.00, 14, 8),
+(75.00, 20, 4),
+(200.00, 8, 2),
+(80.00, 17,6),
+(75.00, 16, 7),
+(100.00, 2, 9),
+(75.00, 17, 2),
+(20.00, 8, 8),
+(150.00, 11, 6),
+(100.00, 5, 1),
+(70.00, 3, 8),
+(64.00, 9, 4),
+(100.00, 7, 10),
+(21.00, 13, 8),
+(400.00, 10, 10),
+(20.00, 15, 5),
+(50.00, 18, 8),
+(29.00, 19, 6),
+(300.00, 19, 10),
